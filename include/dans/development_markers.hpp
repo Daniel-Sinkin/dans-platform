02eebc2c5ef062e823048d36a1df4e7551cb3787
Daniel Sinkin <48285028+OneAndOnlyDaniel@users.noreply.github.com>
2026-06-07 04:56:52 +0200

    add c8, drop cpy marker

--- include/dans/development_markers.hpp
+++ include/dans/development_markers.hpp
@@ -6,30 +6,22 @@
 
 #pragma once
 
-#include <type_traits> // IWYU pragma: keep
-
-#if defined(cpy)
-#error "cpy marker macro is already defined"
-#endif
+#include <type_traits>  // IWYU pragma: keep
 
 #if defined(def)
-#error "def marker macro is already defined"
-#endif
-
-#if defined(__clang__)
-#define cpy [[clang::annotate("cpy")]]
-#else
-#define cpy
+#    error "def marker macro is already defined"
 #endif
 
 // Only use this on the auto for functions when using trailing return
 #define def auto
 
-namespace dans {
+namespace dans
+{
 template <typename T>
-[[nodiscard]] constexpr def copy(const T &value) -> std::remove_cvref_t<T> {
+[[nodiscard]] constexpr def copy(const T& value) -> std::remove_cvref_t<T>
+{
     return value;
 }
-} // namespace dans
+}  // namespace dans
 
-#endif // DANS_CORE_INCLUDE_DANS_DEVELOPMENT_MARKERS_HPP
+#endif  // DANS_CORE_INCLUDE_DANS_DEVELOPMENT_MARKERS_HPP
